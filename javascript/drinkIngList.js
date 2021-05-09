31574e4f521266c2102d1ed6ed8bffa4b1172b1f
Jade D'Souza <jadedsou@uw.edu>
2021-05-09 14:01:49 -0700

    added javascript

---
+++ javascript/drinkIngList.js
@@ -0,0 +1,28 @@
+var ingredients = [
+	"Vodka",
+	"Gin",
+	"Light rum",
+	"Brandy",
+	"Tequila",
+	"Dark rum",
+	"Scotch",
+	"Rum",
+	"Bourbon"];
+
+
+var ingRefresh = [
+	"Vodka",
+	"Bitters",
+	"Galliano",
+	"Lemon peel",
+	"Apple juice",
+	"Soda water",
+	"Tennessee whiskey",
+	"Cream of coconut",
+	"Fruit juice",
+	"Cranberry vodka",
+	"Aquavit",
+	"Dark Creme de Cacao",
+	"Absinthe",
+	"Baileys irish cream",
+	"Raspberry vodka"];
