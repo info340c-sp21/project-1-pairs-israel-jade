011350e997b485c07bac8d3c49125f3acf95296b
Jade D'Souza <jadedsou@uw.edu>
2021-05-10 18:39:00 -0700

    removed js

--- assets/javascript/drinkIngList.js
+++ assets/javascript/drinkIngList.js
@@ -1,28 +1,6 @@
-var ingredients = [
-	"Vodka",
-	"Gin",
-	"Light rum",
-	"Brandy",
-	"Tequila",
-	"Dark rum",
-	"Scotch",
-	"Rum",
-	"Bourbon"];
-
-
-var ingRefresh = [
-	"Vodka",
-	"Bitters",
-	"Galliano",
-	"Lemon peel",
-	"Apple juice",
-	"Soda water",
-	"Tennessee whiskey",
-	"Cream of coconut",
-	"Fruit juice",
-	"Cranberry vodka",
-	"Aquavit",
-	"Dark Creme de Cacao",
-	"Absinthe",
-	"Baileys irish cream",
-	"Raspberry vodka"];
+var moods = [
+	"happy",
+	"sad",
+	"excited",
+	"tired",
+	"angry"];
