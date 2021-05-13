5354bcccb06d7eebbcf4d183345b7791f013a422
Jade D'Souza <jadedsou@uw.edu>
2021-05-13 14:15:59 -0700

    new js

--- javascript/searchLogic.js
+++ javascript/searchLogic.js
@@ -1,25 +1,18 @@
-// input moods
-
-// choose whether want alcoholic or non-alcoholic drink
-
-// get one drink per mood
-
-// angry + alcoholic = "stop"
-
 const DRINKS = {
     alcoholic: [
         {mood:'happy', drink:'Rum & Coke'},
         {mood:'sad', drink:'Red wine'},
         {mood:'excited', drink:'Margarita'},
         {mood:'tired', drink:'Vodka Red Bull'},
-        {mood:'angry', drink:'Uh oh, you should not be drinking alcohol!'}
+        {mood:'angry', drink:'Uh oh, you should not be drinking!'}
     ],
     nonAlcoholic: [
-        {mood:'happy', drink:'Blueberry Smoothie'},
-        {mood:'sad', drink:'Chocolate Milk'},
-        {mood:'excited', drink:'Monster Energy Drink'},
-        {mood:'tired', drink:'Coffee'},
-        {mood:'angry', drink:'Earl Grey Tea'}
+        {mood:'happy', drink:''},
+        {mood:'sad', drink:''},
+        {mood:'excited', drink:''},
+        {mood:'tired', drink:''},
+        {mood:'so-and-so', drink:''},
+        {mood:'angry', drink:''}
     ]
 }
 
@@ -37,26 +30,22 @@ var types = [
 const MOODS = document.querySelector('#moods');
 const TYPES = document.querySelector('#types');
 const ANSWERS = document.querySelector('#answer-list');
-const FORM = document.querySelector('form');
 
-const SUG_MOODS = document.querySelector('#moodSug');
-const SUG_TYPES = document.querySelector('#typeSug');
-const SUG_FORM = document.querySelector('#suggestion-form');
 
-setSelectOptions(moods, MOODS);
-setSelectOptions(types, TYPES);
-setSelectOptions(moods, SUG_MOODS);
-setSelectOptions(types, SUG_TYPES);
+// Set options to select
+moods.forEach(function(mood) {
+    let option = document.createElement('option');
+    option.setAttribute('value', mood);
+    option.appendChild(document.createTextNode(mood));
+    MOODS.append(option);
+});
 
-// function to set all select options
-function setSelectOptions(subject, position) {
-	subject.forEach(function(sub) {
-		let option = document.createElement('option');
-		option.setAttribute('value', sub);
-		option.appendChild(document.createTextNode(sub));
-		position.append(option);
-	});
-}
+types.forEach(function(type) {
+    let option = document.createElement('option');
+    option.setAttribute('value', type);
+    option.appendChild(document.createTextNode(type));
+    document.querySelector('#types').append(option);
+});
 
 // Update left-sidebar with answers
 MOODS.addEventListener('change', function(mood) {
@@ -91,31 +80,9 @@ TYPES.addEventListener('change', function(type) {
 });
 
 
-// For submission
-const DRINKNAME = document.querySelector('#drinkName')
-/*
-FORM.addEventListener('submit', function(event) {
-	event.preventDefault();
-	let pItems = DRINKNAME.getElementsByTagName('p');
-	
-	let selectedMood = MOODS.value;
-	let selectedType = TYPES.value;
-	console.log(renderDrink(selectedType, selectedMood));
-	
-	if (pItems.length == 1) {
-		DRINKNAME.replaceChild(renderDrink(selectedType, selectedMood), pItems[0]);
-	} else {
-		DRINKNAME.appendChild(renderDrink(selectedType, selectedMood));
-	}
 
-	console.log(DRINKNAME);
-});
-*/
 function renderDrink(drinkType, feeling) {
-    if (drinkType == 'non-alcoholic') {
-		drinkType = 'nonAlcoholic';
-	}
-	let getType = DRINKS[drinkType];
+    let getType = DRINKS[drinkType];
     let drink = '';
 
     let i = 0;
@@ -124,11 +91,10 @@ function renderDrink(drinkType, feeling) {
         if (getType[i].mood == feeling) {
             drink = getType[i].drink;
         }
-		i++;
+        console.log(getType[i]);
     });
-	let match = document.createElement('p');
-	//match.setAttribute('display', 'inline-block');
-	match.textContent = drink;
 
-    return match;
+    return drink;
 }
+
+console.log(renderDrink('alcoholic', 'angry'));
