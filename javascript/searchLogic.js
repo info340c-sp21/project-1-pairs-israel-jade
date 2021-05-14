35a01d189ee8e0762fbd5a844eb61c18b1fa1022
Israel Martinez <israel.r.mar@gmail.com>
2021-05-14 15:46:24 -0700

    added setChangeEvents fn for simpler code

--- javascript/searchLogic.js
+++ javascript/searchLogic.js
@@ -8,18 +8,18 @@
 
 const DRINKS = {
     alcoholic: [
-        {mood:'happy', drink:'Rum & Coke'},
-        {mood:'sad', drink:'Red wine'},
-        {mood:'excited', drink:'Margarita'},
-        {mood:'tired', drink:'Vodka Red Bull'},
-        {mood:'angry', drink:'Uh oh, you should not be drinking alcohol!'}
+        {mood:'happy', drink:'Cuba Libre', author:'The Spruce Eats', link:'https://www.thespruceeats.com/rum-and-coke-recipe-760560'},
+        {mood:'sad', drink:'The Whiskey Sour', author:'The Spruce Eats', link:'https://www.thespruceeats.com/whiskey-sour-recipe-761273'},
+        {mood:'excited', drink:'Blue Margarita', author:'The Spruce Eats', link:'https://www.thespruceeats.com/blue-margarita-recipe-760849'},
+        {mood:'tired', drink:'Vodka Red Bull', author:'Liqour.com', link:'https://www.liquor.com/recipes/vodka-red-bull/'},
+        {mood:'angry', drink:'Uh oh, you should not be drinking alcohol! Please pick another mood or drink type.'}
     ],
     nonAlcoholic: [
-        {mood:'happy', drink:'Blueberry Smoothie'},
-        {mood:'sad', drink:'Chocolate Milk'},
-        {mood:'excited', drink:'Monster Energy Drink'},
-        {mood:'tired', drink:'Coffee'},
-        {mood:'angry', drink:'Earl Grey Tea'}
+        {mood:'happy', drink:'Low-Calorie Blueberry Smoothie', author:'The Spruce Eats', link:'https://www.thespruceeats.com/blueberry-smoothie-recipe-2238438'},
+        {mood:'sad', drink:'Chocolate Milk', author:'The Spruce Eats', link:'https://www.thespruceeats.com/chocolate-milk-recipe-2355494'},
+        {mood:'excited', drink:'Fresh Watermelon Lemonade', author:'The Spruce Eats', link:'https://www.thespruceeats.com/fresh-watermelon-lemonade-5071240'},
+        {mood:'tired', drink:'Chocolate Coffee Protein Shake', author:'The Spruce Eats', link:'https://www.thespruceeats.com/chocolate-coffee-protein-shake-4031922'},
+        {mood:'angry', drink:'Earl Grey Tea', author:'The Spruce Eats', link:'https://www.31daily.com/how-to-brew-earl-grey-tea/'}
     ]
 }
 
@@ -42,12 +42,13 @@ const SUG_MOODS = document.querySelector('#moodSug');
 const SUG_TYPES = document.querySelector('#typeSug');
 const SUG_FORM = document.querySelector('#suggestion-form');
 
+
+// function to set all select options
 setSelectOptions(moods, MOODS);
 setSelectOptions(types, TYPES);
 setSelectOptions(moods, SUG_MOODS);
 setSelectOptions(types, SUG_TYPES);
 
-// function to set all select options
 function setSelectOptions(subject, position) {
 	subject.forEach(function(sub) {
 		let option = document.createElement('option');
@@ -57,37 +58,30 @@ function setSelectOptions(subject, position) {
 	});
 }
 
-// Update left-sidebar with answers
-MOODS.addEventListener('change', function(mood) {
-    let listItems = ANSWERS.getElementsByTagName('li');
-    
-    let select = mood.target;
-    let value = select.value;
-    
-    let answer = document.createElement('li');
-    answer.textContent = value;
-    if (listItems.length == 2) {
-        ANSWERS.replaceChild(answer, listItems[0]);
-    } else {
-        ANSWERS.appendChild(answer);
-    }
-});
 
-TYPES.addEventListener('change', function(type) {
-    let listItems = ANSWERS.getElementsByTagName('li');
-    
-    let select = type.target;
-    let value = select.value;
-    
-    let answer = document.createElement('li');
-    answer.textContent = value;
+// function to update left-sidebar with answers
+setChangeEvents(MOODS);
+setChangeEvents(TYPES);
 
-    if (listItems.length == 2) {
-        ANSWERS.replaceChild(answer, listItems[1]);
-    } else {
-        ANSWERS.appendChild(answer);
-    }
-});
+function setChangeEvents(options) {
+    options.addEventListener('change', function(option) {
+        let listItems = ANSWERS.getElementsByTagName('li');
+        
+        let select = option.target;
+        let value = select.value;
+
+        let answer = document.createElement('li');
+        answer.textContent = value;
+
+        if (listItems.length == 2 && options == MOODS) {
+            ANSWERS.replaceChild(answer, listItems[0]);
+        } else if (listItems.length == 2 && options == TYPES) {
+            ANSWERS.replaceChild(answer, listItems[1]);
+        } else {
+            ANSWERS.appendChild(answer);
+        }
+    });
+}
 
 
 // For submission
@@ -126,15 +120,11 @@ function randomDrink() {
 	let drinkKeys = Object.keys(DRINKS);
 	let num = Math.floor(Math.random() * drinkKeys.length);
 	let drinkType = drinkKeys[num];
-	console.log(num + ' ' + drinkType);
-
 	num = Math.floor(Math.random() * DRINKS[drinkType].length)
 	if (drinkType == 'alcoholic' && num == 4) {
 		num = num - 1;
 	}
 
-	console.log(num);
-	console.log(DRINKS[drinkType][num].mood);
     return renderDrink(drinkType, DRINKS[drinkType][num].mood);
 }
 
@@ -144,12 +134,14 @@ function renderDrink(drinkType, feeling) {
 	}
 	let getType = DRINKS[drinkType];
     let drink = '';
+    let link = '';
 
     let i = 0;
 
     getType.forEach(function() {
         if (getType[i].mood == feeling) {
             drink = getType[i].drink;
+            link = getType[i].link;
         }
 		i++;
     });
